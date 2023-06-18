f65a0085d19f9ce6b939297de634abc220d0566c
maheshmahi24 <133503963+maheshmahi24@users.noreply.github.com>
2023-06-18 12:18:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com
 docker push 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com/automation-docker:$1
-docker rm -f newrepo-app | ls -la
+docker rm -f dotnet-app | ls -la
 docker ps -a
 docker run -itd -p 3000:3000 --name dockerdeploy 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com:$1
 
